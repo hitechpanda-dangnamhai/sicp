4068b241ab2abca957be6e2331ddd78984863740
hitechpanda-dangnamhai <ID+hitechpanda-dangnamhai@users.noreply.github.com>
2026-06-09 08:44:28 +0700

    fix dont upload onnx

---
+++ packages/shared-types/src/api/models/TrackBatchDto.ts
@@ -0,0 +1,186 @@
+/* generated using openapi-typescript-codegen -- do not edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+export type TrackBatchDto = {
+  events: Array<({
+    event_id: string;
+    occurred_at: string;
+    received_at?: string;
+    user_id?: string;
+    session_id: string;
+    device_id?: string;
+    intent?: string;
+    modality?: 'text' | 'voice' | 'image';
+    request_id?: string;
+    subject_type?: 'product' | 'cart' | 'order' | 'category' | 'query' | 'card';
+    subject_id?: string;
+    user_agent?: string;
+    ip_hash?: string;
+    app_version: string;
+    event_type: 'session.started';
+    properties: {
+      source: 'web' | 'mobile';
+    };
+  } | {
+    event_id: string;
+    occurred_at: string;
+    received_at?: string;
+    user_id?: string;
+    session_id: string;
+    device_id?: string;
+    intent?: string;
+    modality?: 'text' | 'voice' | 'image';
+    request_id?: string;
+    subject_type?: 'product' | 'cart' | 'order' | 'category' | 'query' | 'card';
+    subject_id?: string;
+    user_agent?: string;
+    ip_hash?: string;
+    app_version: string;
+    event_type: 'product.viewed';
+    properties: {
+      product_id: string;
+      source: 'search' | 'reco' | 'cart' | 'direct';
+      dwell_ms?: number;
+    };
+  } | {
+    event_id: string;
+    occurred_at: string;
+    received_at?: string;
+    user_id?: string;
+    session_id: string;
+    device_id?: string;
+    intent?: string;
+    modality?: 'text' | 'voice' | 'image';
+    request_id?: string;
+    subject_type?: 'product' | 'cart' | 'order' | 'category' | 'query' | 'card';
+    subject_id?: string;
+    user_agent?: string;
+    ip_hash?: string;
+    app_version: string;
+    event_type: 'cart.item_added';
+    properties: {
+      product_id: string;
+      qty: number;
+      unit_price: number;
+      source: 'search' | 'reco' | 'voice' | 'direct';
+      from_query?: string;
+    };
+  } | {
+    event_id: string;
+    occurred_at: string;
+    received_at?: string;
+    user_id?: string;
+    session_id: string;
+    device_id?: string;
+    intent?: string;
+    modality?: 'text' | 'voice' | 'image';
+    request_id?: string;
+    subject_type?: 'product' | 'cart' | 'order' | 'category' | 'query' | 'card';
+    subject_id?: string;
+    user_agent?: string;
+    ip_hash?: string;
+    app_version: string;
+    event_type: 'auth.signed_in';
+    properties: {
+      method: 'password';
+    };
+  } | {
+    event_id: string;
+    occurred_at: string;
+    received_at?: string;
+    user_id?: string;
+    session_id: string;
+    device_id?: string;
+    intent?: string;
+    modality?: 'text' | 'voice' | 'image';
+    request_id?: string;
+    subject_type?: 'product' | 'cart' | 'order' | 'category' | 'query' | 'card';
+    subject_id?: string;
+    user_agent?: string;
+    ip_hash?: string;
+    app_version: string;
+    event_type: 'auth.signed_out';
+    properties: any;
+  } | {
+    event_id: string;
+    occurred_at: string;
+    received_at?: string;
+    user_id?: string;
+    session_id: string;
+    device_id?: string;
+    intent?: string;
+    modality?: 'text' | 'voice' | 'image';
+    request_id?: string;
+    subject_type?: 'product' | 'cart' | 'order' | 'category' | 'query' | 'card';
+    subject_id?: string;
+    user_agent?: string;
+    ip_hash?: string;
+    app_version: string;
+    event_type: 'auth.password_reset_requested';
+    properties: {
+      email_hash: string;
+    };
+  } | {
+    event_id: string;
+    occurred_at: string;
+    received_at?: string;
+    user_id?: string;
+    session_id: string;
+    device_id?: string;
+    intent?: string;
+    modality?: 'text' | 'voice' | 'image';
+    request_id?: string;
+    subject_type?: 'product' | 'cart' | 'order' | 'category' | 'query' | 'card';
+    subject_id?: string;
+    user_agent?: string;
+    ip_hash?: string;
+    app_version: string;
+    event_type: 'nav.settings_section_opened';
+    properties: {
+      section: 'notifications' | 'security' | 'help';
+    };
+  } | {
+    event_id: string;
+    occurred_at: string;
+    received_at?: string;
+    user_id?: string;
+    session_id: string;
+    device_id?: string;
+    intent?: string;
+    modality?: 'text' | 'voice' | 'image';
+    request_id?: string;
+    subject_type?: 'product' | 'cart' | 'order' | 'category' | 'query' | 'card';
+    subject_id?: string;
+    user_agent?: string;
+    ip_hash?: string;
+    app_version: string;
+    event_type: 'error.report_requested';
+    properties: {
+      trace_id: string;
+      error_code: string;
+    };
+  } | {
+    event_id: string;
+    occurred_at: string;
+    received_at?: string;
+    user_id?: string;
+    session_id: string;
+    device_id?: string;
+    intent?: string;
+    modality?: 'text' | 'voice' | 'image';
+    request_id?: string;
+    subject_type?: 'product' | 'cart' | 'order' | 'category' | 'query' | 'card';
+    subject_id?: string;
+    user_agent?: string;
+    ip_hash?: string;
+    app_version: string;
+    event_type: 'nav.tile_clicked';
+    properties: {
+      tile_id: 'nhap_hang' | 'phan_tich' | 'tim_san_pham' | 'mua_hang' | 'goi_y_san_pham' | 'gio_hang';
+      intent_id: 'intent-01' | 'intent-02' | 'intent-03' | 'intent-04' | 'intent-05' | 'intent-07';
+      source: 'hero_tile' | 'list_tile';
+    };
+  })>;
+};
+
